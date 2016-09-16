0c33ff45bc8c1f991247d1c001c38eadfbac3345
Barry <barrylockwood.swdev@gmail.com>
2016-09-15 17:47:47 -0700

    Cleaning up a few things

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'nJysGb51dyHNMErUv900bjv8U',
-  consumer_secret:      'C9IuZDLLSrGUtFs0dybOpknzjuLN53pRNiTK5yqsGCD7cXiZ69',
-  access_token:         '774744063245299712-9uONoKn7tCZloc5iKbODLBo50uIXXI8',
-  access_token_secret:  'keW9fdit0sVGYsgd6iMCGe39pSoQwckVZ2n2VNfal61Zc'
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 }
